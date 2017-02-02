d6ee13783e6df6f6cfaf094346f2e6c04e869a20
hjmin <agapehj.min@gmail.com>
2017-02-02 16:40:10 +0900

    static change

--- Calculator.java
+++ Calculator.java
@@ -2,12 +2,20 @@
 
 public class Calculator {
 
-	public int add(int x, int y) {
+	public static int add(int x, int y) {
 		return x+y;
 	}
 
-	public int substract(int x, int y) {
+	public static int substract(int x, int y) {
 		return x-y;
+	}
+
+	public static int multiply(int x, int y) {
+		return x * y;
+	}
+	
+	public static int divide(int x, int y) {
+		return x / y;
 	}
 
 }
